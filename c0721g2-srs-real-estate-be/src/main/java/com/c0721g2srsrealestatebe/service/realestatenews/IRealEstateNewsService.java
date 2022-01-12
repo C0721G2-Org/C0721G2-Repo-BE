4e7fe3afa0dedc53d2d75872aa825a18384542f1
Namtranvn1991 <namtranvn1991@gmail.com>
2022-01-12 08:23:22 +0700

    123

--- c0721g2-srs-real-estate-be/src/main/java/com/c0721g2srsrealestatebe/service/realestatenews/IRealEstateNewsService.java
+++ c0721g2-srs-real-estate-be/src/main/java/com/c0721g2srsrealestatebe/service/realestatenews/IRealEstateNewsService.java
@@ -3,9 +3,6 @@ package com.c0721g2srsrealestatebe.service.realestatenews;
 import com.c0721g2srsrealestatebe.model.realestatenews.RealEstateNews;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
@@ -39,5 +36,4 @@ public interface IRealEstateNewsService {
                                                              Pageable pageable);
 
     RealEstateNews saveRealEstateNews(RealEstateNews realEstateNews);
-
 }
