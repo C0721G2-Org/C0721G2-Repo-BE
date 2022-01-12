93dd5e19da68d4e6db8fd69b672c157bf5b2d5b1
Namtranvn1991 <namtranvn1991@gmail.com>
2022-01-12 19:33:12 +0700

    123

--- c0721g2-srs-real-estate-be/src/main/java/com/c0721g2srsrealestatebe/service/realestatenews/IDirectionService.java
+++ c0721g2-srs-real-estate-be/src/main/java/com/c0721g2srsrealestatebe/service/realestatenews/IDirectionService.java
@@ -1,4 +1,9 @@
 package com.c0721g2srsrealestatebe.service.realestatenews;
 
+import com.c0721g2srsrealestatebe.model.realestatenews.Direction;
+
+import java.util.List;
+
 public interface IDirectionService {
+    List<Direction> directionList();
 }
